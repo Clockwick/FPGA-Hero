686240ee4270ac2b5700f6e525a51f2428de303d
PIMPIYAJIRANAN <pim_kiko@hotmail.com>
2020-12-14 22:43:18 +0700

    Texture

--- MainMenu.py
+++ MainMenu.py
@@ -1,9 +1,12 @@
 # Own python
 from State import State
+from Texture import Texture
 
 # Pygame
 import pygame
 import pygame_gui
+import os
+
 
 class MainMenuState(State):
     def __init__(self, manager,game_data):
@@ -14,14 +17,56 @@ class MainMenuState(State):
         self.W = resolution[0]
         self.H = resolution[1]
 
-        self.button_width = self.W * 1/8
-        self.button_height = self.H * 1/12
-        
+        self.button_width = int(self.W * 1/8)
+        self.button_height = int(self.H * 1/12)
+
+        self.space_y=self.H//8
+
+        self.mainmenu_button_1P_x=self.W//2
+        self.mainmenu_button_1P_y=self.H//2.5 
+
+        self.mainmenu_button_2P_x=self.W//2
+        self.mainmenu_button_2P_y=self.H//2.5 + self.space_y
+
+        self.mainmenu_howtoplay_x=self.W//2
+        self.mainmenu_howtoplay_y=self.H//2.5 + self.space_y*2
+
+        self.mainmenu_button_1P=pygame.transform.scale(pygame.image.load(os.path.join("assets","buttunp1-01.png")),(self.button_width,self.button_height))
+        self.mainmenu_button_1P_click=pygame.transform.scale(pygame.image.load(os.path.join("assets","p1hold-01.png")),(self.button_width,self.button_height))
+        self.mainmenu_button_2P=pygame.transform.scale(pygame.image.load(os.path.join("assets","buttunp2-01.png")),(self.button_width,self.button_height))
+        self.mainmenu_button_2P_click=pygame.transform.scale(pygame.image.load(os.path.join("assets","p2hold-01.png")),(self.button_width,self.button_height))
+        self.mainmenu_button_howtoplay=pygame.transform.scale(pygame.image.load(os.path.join("assets","buttunhtp-01.png")),(self.button_width,self.button_height))
+        self.mainmenu_button_howtoplay_click=pygame.transform.scale(pygame.image.load(os.path.join("assets","htphold-01.png")),(self.button_width,self.button_height))
+       
+
+        self.mainmenu_button_1P_data={
+            "ideal_img" : self.mainmenu_button_1P,
+            "active_img" : self.mainmenu_button_1P_click,
+            "position" : (self.mainmenu_button_1P_x,self.mainmenu_button_1P_y)
+        }
+
+        self.mainmenu_button_2P_data={
+            "ideal_img" : self.mainmenu_button_2P,
+            "active_img" : self.mainmenu_button_2P_click,
+            "position" : (self.mainmenu_button_2P_x,self.mainmenu_button_2P_y)
+        }
+
+        self.mainmenu_button_howtoplay_data={
+            "ideal_img" : self.mainmenu_button_howtoplay,
+            "active_img" : self.mainmenu_button_howtoplay_click,
+            "position" : (self.mainmenu_howtoplay_x,self.mainmenu_howtoplay_y)
+        }
+
+        self.mainmenu_button_1P_texture=Texture(self.mainmenu_button_1P_data)
+        self.mainmenu_button_2P_texture=Texture(self.mainmenu_button_2P_data)
+        self.mainmenu_button_howtoplay_texture=Texture(self.mainmenu_button_howtoplay_data)
+
+        self.mainmenu_bg=pygame.transform.scale(pygame.image.load(os.path.join("assets","mainmenu_bg.png")),(self.W,self.H))
         self.manager = manager
         if full_screen:
-            self.mainmenu_bg = pygame.Surface((0,0),pygame.FULLSCREEN)
+            self.mainmenu_bg_sur = pygame.Surface((0,0),pygame.FULLSCREEN)
         else:
-            self.mainmenu_bg = pygame.Surface(resolution)
+            self.mainmenu_bg_sur = pygame.Surface(resolution)
 
         self.main_font = pygame.font.SysFont("roboto", 30)
 
@@ -39,7 +84,14 @@ class MainMenuState(State):
         self.manager.update(time_delta)
     def render(self,window):
         window.blit(self.mainmenu_bg, (0, 0))
-        self.manager.draw_ui(window)
+        window.blit(self.mainmenu_bg,(0,0))
+        # self.mainmenu_button_1P_texture.render(self.mainmenu_bg_sur)
+        # self.mainmenu_button_2P_texture.render(self.mainmenu_bg_sur)
+        # self.mainmenu_button_howtoplay_texture.render(self.mainmenu_bg_sur)
+        self.mainmenu_button_1P_texture.render(window)
+        self.mainmenu_button_2P_texture.render(window)
+        self.mainmenu_button_howtoplay_texture.render(window)
+        # self.manager.draw_ui(window)
 
     def get1PBtn(self):
         return self._1p_btn
---
+++ Texture.py
@@ -0,0 +1,16 @@
+import pygame
+
+
+class Texture:
+    def __init__(self,texture_data):
+        self.texture_dict=texture_data
+        self.ideal_image=texture_data["ideal_img"]
+        self.active_image=texture_data["active_img"]
+        self.position_image=texture_data["position"]
+        self.ideal_image_sur=pygame.Surface((int(self.ideal_image.get_width()),int(self.ideal_image.get_height())))
+        self.active_image_sur=pygame.Surface((int(self.active_image.get_width()),int(self.active_image.get_height())))
+    def update(self):
+        pass
+    def render(self,window):
+        window.blit(self.ideal_image,(self.position_image[0]-self.ideal_image.get_width()//2,self.position_image[1]-self.ideal_image.get_height()//2))
+        
